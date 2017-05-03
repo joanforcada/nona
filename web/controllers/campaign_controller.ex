70aad760a3a5c118f32e91e0eaf690c67b694b38
Oscar <amrothceleb@gmail.com>
2017-05-03 17:57:39 +0200

    campaign and currency controllers populated

---
+++ web/controllers/campaign_controller.ex
@@ -0,0 +1,19 @@
+defmodule Tino.CampaignController do
+  use Tino.Web, :controller
+
+  def new do
+    
+  end
+
+  def create do
+
+  end
+
+  def edit do
+
+  end
+
+  def update do
+
+  end
+end
